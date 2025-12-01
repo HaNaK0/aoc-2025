6bdb6d3fc363b1a34be7c99fdf03b41877bef243
HaNaK0 <hampus@hampushuledal.com>
2025-12-01 22:07:27 +0100

    merge code that i might want to reuse and add readme
    add readme
    merge read input function
    merge cargo.lock
    add empty input.txt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+
+const INPUT_PATH: &str = "input.txt";
+
 fn main() {
-    println!("Hello, world!");
 }
+
+/// Reads the input into a iterator returning Strings
+/// Panics if there is an issue with the file or a line
+fn read_input() -> impl Iterator<Item = String> {
+    let file = File::open(INPUT_PATH).expect("Failed to read file");
+    let buf_read = BufReader::new(file);
+    buf_read.lines().map(|r| r.expect("failed to read line"))
+}
+
+
