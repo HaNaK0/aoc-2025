3767c8f4466cf9f9602e634f3eb099c19ae92139
HaNaK0 <hampus@hampushuledal.com>
2025-12-07 20:35:04 +0100

    day 3 task 2

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::{
 const INPUT_PATH: &str = "input.txt";
 
 fn main() {
-    let res: u32 = read_input().map(|l| handle_bank(&l)).sum();
+    let res: u128 = read_input().map(|l| handle_bank(&l)).sum();
 
     println!("{res}");
 }
@@ -19,33 +19,27 @@ fn read_input() -> impl Iterator<Item = String> {
     buf_read.lines().map(|r| r.expect("failed to read line"))
 }
 
-fn handle_bank(bank: &str) -> u32 {
-    let mut first_battery = bank.chars().next().unwrap();
+fn handle_bank(bank: &str) -> u128 {
+    let mut full_bank = ['0'; 12];
     let mut current_index = 0;
 
-    for (index, battery) in bank.char_indices().take(bank.len() - 1).skip(1) {
-        if battery > first_battery {
-            first_battery = battery;
-            current_index = index;
-        }
-        if battery == '9' {
-            break;
-        }
-    }
-
-    let mut second_battery = '0';
-
-    for battery in bank[current_index + 1..].chars() {
-        if battery > second_battery {
-            second_battery = battery;
+    for (i, current_battery) in full_bank.iter_mut().enumerate() {
+        for (index, battery) in bank
+            .char_indices()
+            .take(bank.len() - (11 - i))
+            .skip(current_index)
+        {
+            if battery > *current_battery {
+                *current_battery = battery;
+                current_index = index + 1;
+            }
+            if battery == '9' {
+                break;
+            }
         }
     }
 
-    let full_bank = [first_battery, second_battery]
-        .iter()
-        .collect::<String>()
-        .parse()
-        .unwrap();
+    let full_bank = full_bank.iter().collect::<String>().parse().unwrap();
     println!("{full_bank}");
     full_bank
 }
