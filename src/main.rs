aeda69549971111f4e03ca2533235c2cd06d5304
HaNaK0 <hampus@hampushuledal.com>
2025-12-07 07:59:51 +0100

    day 3 task 1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use std::{fs::File, io::{BufRead, BufReader}};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 const INPUT_PATH: &str = "input.txt";
 
 fn main() {
+    let res: u32 = read_input().map(|l| handle_bank(&l)).sum();
+
+    println!("{res}");
 }
 
 /// Reads the input into a iterator returning Strings
@@ -13,4 +19,33 @@ fn read_input() -> impl Iterator<Item = String> {
     buf_read.lines().map(|r| r.expect("failed to read line"))
 }
 
+fn handle_bank(bank: &str) -> u32 {
+    let mut first_battery = bank.chars().next().unwrap();
+    let mut current_index = 0;
 
+    for (index, battery) in bank.char_indices().take(bank.len() - 1).skip(1) {
+        if battery > first_battery {
+            first_battery = battery;
+            current_index = index;
+        }
+        if battery == '9' {
+            break;
+        }
+    }
+
+    let mut second_battery = '0';
+
+    for battery in bank[current_index + 1..].chars() {
+        if battery > second_battery {
+            second_battery = battery;
+        }
+    }
+
+    let full_bank = [first_battery, second_battery]
+        .iter()
+        .collect::<String>()
+        .parse()
+        .unwrap();
+    println!("{full_bank}");
+    full_bank
+}
