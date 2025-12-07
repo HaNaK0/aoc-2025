1020659b3eef8884f8c9a1c1733f8751d5c3fd0f
HaNaK0 <hampus@hampushuledal.com>
2025-12-07 22:12:18 +0100

    add a position type
    add position struct
    implement display for that struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
-use std::{fs::File, io::{BufRead, BufReader}};
+use std::{
+    fmt::Display,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 const INPUT_PATH: &str = "input.txt";
 
-fn main() {
+struct Pos<T> 
+{
+    pub x: T,
+    pub y: T,
 }
+
+impl<T> Display for Pos<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Pos(x:{},y:{})", self.x, self.y)
+    }
+}
+
+fn main() {}
 
 /// Reads the input into a iterator returning Strings
 /// Panics if there is an issue with the file or a line
@@ -12,5 +30,3 @@ fn read_input() -> impl Iterator<Item = String> {
     let buf_read = BufReader::new(file);
     buf_read.lines().map(|r| r.expect("failed to read line"))
 }
-
-
