55da69119ff35ea2b82deeb3456f7c498c56e367
HaNaK0 <hampus@hampushuledal.com>
2025-12-08 20:45:48 +0100

    day 4 part 1 completed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     fmt::Display,
     fs::File,
     io::{BufRead, BufReader},
@@ -6,8 +7,8 @@ use std::{
 
 const INPUT_PATH: &str = "input.txt";
 
-struct Pos<T> 
-{
+#[derive(PartialEq, Eq, Hash)]
+struct Pos<T> {
     pub x: T,
     pub y: T,
 }
@@ -21,7 +22,49 @@ where
     }
 }
 
-fn main() {}
+fn main() {
+    // read the position of the paper rolls into a set
+    let rolls: HashSet<Pos<i32>> = read_input()
+        .enumerate()
+        .flat_map(move |(y, l)| {
+            l.char_indices()
+                .map(|(x, c)| {
+                    (
+                        c,
+                        Pos {
+                            x: x as i32,
+                            y: y as i32,
+                        },
+                    )
+                })
+                .collect::<Vec<(char, Pos<i32>)>>()
+        })
+        .filter_map(|(c, p)| if c == '@' { Some(p) } else { None })
+        .collect();
+
+    let res = rolls
+        .iter()
+        .map(|r| {
+            (-1..2)
+                .flat_map(|x| (-1..2).map(move |y| (x, y)))
+                .filter_map(|(x, y)| {
+                    if x != 0 || y != 0 {
+                        Some(Pos {
+                            x: r.x + x,
+                            y: r.y + y,
+                        })
+                    } else {
+                        None
+                    }
+                })
+                .filter(|p| rolls.contains(p))
+                .count()
+        })
+        .filter(|n| *n < 4)
+        .count();
+
+    println!("amount of rolls:{}", res)
+}
 
 /// Reads the input into a iterator returning Strings
 /// Panics if there is an issue with the file or a line
