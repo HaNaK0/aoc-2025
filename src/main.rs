d0a717826680ab9084f6165de0209e4aa2c871fb
HaNaK0 <hampus@hampushuledal.com>
2025-12-09 15:52:25 +0100

    day 5 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,75 @@
-use std::{fs::File, io::{BufRead, BufReader}};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 const INPUT_PATH: &str = "input.txt";
 
+struct Span {
+    start: usize,
+    end: usize,
+}
+
+impl Span {
+    pub fn new(line: &str) -> Self {
+        let mut split = line.split('-');
+        Self {
+            start: split.next().unwrap().parse().unwrap(),
+            end: split.next().unwrap().parse().unwrap(),
+        }
+    }
+
+    pub fn contains(&self, num: usize) -> bool {
+        self.start <= num && self.end >= num
+    }
+
+    pub fn merge(&mut self, other: &Self) -> bool {
+        if self.end < other.start || self.start > other.end {
+            false
+        } else {
+            self.start = if self.start < other.start {
+                self.start
+            } else {
+                other.start
+            };
+            self.end = if self.end > other.end {
+                self.end
+            } else {
+                other.end
+            };
+
+            true
+        }
+    }
+}
+
 fn main() {
+    let mut input = read_input();
+
+    let mut line = input.next().unwrap();
+    let mut spans: Vec<Span> = Vec::new();
+
+    while !line.is_empty() {
+        let new_span = Span::new(&line);
+
+        let mut overlap = false;
+        for span in &mut spans {
+            if span.merge(&new_span) {
+                overlap = true;
+                break;
+            }
+        }
+        if !overlap {
+            spans.push(new_span);
+        }
+        line = input.next().unwrap();
+    }
+
+    let res = input.filter(|l| {
+        let ingredient: usize = l.parse().expect("failed to parse ingredient");
+        spans.iter().any(|s| s.contains(ingredient))
+    }).count();
+    println!("result: {res}")
 }
 
 /// Reads the input into a iterator returning Strings
@@ -12,5 +79,3 @@ fn read_input() -> impl Iterator<Item = String> {
     let buf_read = BufReader::new(file);
     buf_read.lines().map(|r| r.expect("failed to read line"))
 }
-
-
