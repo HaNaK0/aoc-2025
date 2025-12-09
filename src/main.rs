16a1cd49ba99deb4321a8235fd62c3c0477bb424
HaNaK0 <hampus@hampushuledal.com>
2025-12-09 19:22:19 +0100

    day 6 part 1

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,23 @@ use std::{fs::File, io::{BufRead, BufReader}};
 const INPUT_PATH: &str = "input.txt";
 
 fn main() {
+    let lines: Vec<String> = read_input().collect();
+
+    let operators: Vec<&str> = lines.last().unwrap().split_whitespace().collect();
+
+    let results: Vec<u128> = lines[..lines.len() - 1].iter()
+        .map(|l| l.split_whitespace().map(|s| s.parse::<u128>().unwrap()).collect::<Vec<u128>>())
+        .reduce(|res, next| {
+            res.iter().zip(next.iter()).zip(operators.iter()).map(|((p, n), o)| {
+                match *o {
+                    "+" => p + n,
+                    "*" => p * n,
+                    _ => panic!("invalid operator")
+                }
+            }).collect()
+        }).unwrap();
+
+    println!("{:?}", results.iter().sum::<u128>())
 }
 
 /// Reads the input into a iterator returning Strings
@@ -12,5 +29,4 @@ fn read_input() -> impl Iterator<Item = String> {
     let buf_read = BufReader::new(file);
     buf_read.lines().map(|r| r.expect("failed to read line"))
 }
-
 
